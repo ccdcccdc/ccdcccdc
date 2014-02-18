0263b5e6b6d388cdf7e3e32b4278b65777eb62fb
Matt Thompson <empty1@gmail.com>
2014-02-17 22:04:59 -0600

    added unzip and tiger install

--- LinuxHarden/apt-update.sh
+++ LinuxHarden/apt-update.sh
@@ -4,6 +4,8 @@
 sed -i -e 's/us.archive.ubuntu.com\|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
 
 # update system
-apt-get update -y && apt-get dist-upgrade -y
+apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y
 
+#install unzip for scripts, and tiger for hardening
 
+apt-get install unzip tiger 
