d76111c38ca45bf465a808e727a18717e27c6d39
Matt Thompson <empty1@gmail.com>
2014-02-17 21:47:17 -0600

    cleaning up my mess :)

--- apt-update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#replace old sources list with updated version
-sed -i -e 's/us.archive.ubuntu.com\|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
-
-# update system
-apt-get update -y && apt-get dist-upgrade -y
-
-
